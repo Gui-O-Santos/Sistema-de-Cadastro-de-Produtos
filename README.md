b86fcdb8f42cb2f8b76c9b96fdd5a0786109f80c
Guilherme Oliveira Santos <217628894+Gui-O-Santos@users.noreply.github.com>
2026-09-16 19:39:15 +0000

    Configurações

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Sistema-de-Cadastro-de-Produtos
+
+## Configurações
+```
+chmod +x configurações.sh
+./configurações.sh
+```
+## Execução
+```
+./execução.sh
+```
---
+++ configurações.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install php -y
+php -v
+chmod +x execução.sh
+sudo apt install php-mysql
+cd /tmp
+wget https://www.php.net/distributions/php-8.0.30.tar.gz
+tar -xzf php-8.0.30.tar.gz
+clear
+echo "Instalação Concluida"
